f9f203a2e5f43c4b82f16698feaa5e5138b28036
Dark-Existed <c2572388311@gmail.com>
2018-11-13 00:23:54 +0800

    feat: add BaiduMap sdk

--- app/src/main/java/com/bbel/eatnow/MainActivity.java
+++ app/src/main/java/com/bbel/eatnow/MainActivity.java
@@ -3,11 +3,47 @@ package com.bbel.eatnow;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.baidu.mapapi.CoordType;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
+
 public class MainActivity extends AppCompatActivity {
+
+    MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
+        SDKInitializer.setCoordType(CoordType.BD09LL);
         setContentView(R.layout.activity_main);
+
+        initWidgets();
     }
+
+
+    private void initWidgets() {
+        mapView = findViewById(R.id.mmap);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
 }
