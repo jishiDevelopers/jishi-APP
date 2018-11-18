ad6b6926e7c364fb3c8a93cf69c564a6f602bd17
Snow <601537645@qq.com>
2018-11-18 20:31:51 +0800

    feat:abandon toobar

--- app/src/main/java/com/bbel/eatnow/MainActivity.java
+++ app/src/main/java/com/bbel/eatnow/MainActivity.java
@@ -4,12 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.support.v7.widget.Toolbar;
-import android.widget.Toast;
+
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -18,7 +16,6 @@ import okhttp3.Response;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private String restaurant;
     private String dishes;
-
     public void setRestaurant(String restaurant) {
         this.restaurant = restaurant;
     }
@@ -30,29 +27,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private String demoDishes;
 
     private void setDemoDishes(String dishs) {
-        this.demoDishes ="《"+dishs + "》吧";
+        this.demoDishes = "《" + dishs + "》吧";
     }
 
     private String demoRestaurant;
 
-    private void setDemoRestaurant(String restaurant) {
+    public void setDemoRestaurant(String restaurant) {
         this.demoRestaurant = "今天就吃《" + restaurant + "》的:";
     }
 
     private int which;
+
     public void addWhich(int which) {
 
-        this.which=++which;
+        this.which = ++which;
         if (which > 3) {
             which = which % 4;
-            this.which=which;
+            this.which = which;
         }
 
     }
     public void setWhich(int which) {
         this.which = which;
     }
-
     TextView tvRecommendDishes;
     TextView tvRecommendRestaurant;
     private String[] restaurantArray = {"腊么香", "港式秘密", "新概念", "小米米"};
@@ -62,13 +59,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         Button btRecommendGo = (Button) findViewById(R.id.bt_recommend_go);
         Button btRecommendNext = (Button) findViewById(R.id.bt_recommend_next_restaurant);
-        Button btChooseFitst =(Button)findViewById(R.id.bt_recommend_first) ;
-        Button btChooseSecond=(Button)findViewById(R.id.bt_recommend_second);
-        Button btChooseThird=(Button)findViewById(R.id.bt_recommend_third);
-        Button btChooseFourth=(Button)findViewById(R.id.bt_recommend_fourth);
+        Button btChooseFitst = (Button) findViewById(R.id.bt_recommend_first);
+        Button btChooseSecond = (Button) findViewById(R.id.bt_recommend_second);
+        Button btChooseThird = (Button) findViewById(R.id.bt_recommend_third);
+        Button btChooseFourth = (Button) findViewById(R.id.bt_recommend_fourth);
         btChooseFitst.setOnClickListener(this);
         btChooseSecond.setOnClickListener(this);
         btChooseThird.setOnClickListener(this);
@@ -84,9 +80,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setDemoDishes(dishes);
         tvRecommendRestaurant.setText(demoRestaurant);
         tvRecommendDishes.setText(demoDishes);
-        setSupportActionBar(toolbar);
     }
-
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -102,22 +96,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(intent);
                 break;
             }
-            case R.id.bt_recommend_first:{
+            case R.id.bt_recommend_first: {
                 setWhich(0);
                 setResultRecommend(which);
                 break;
             }
-            case R.id.bt_recommend_second:{
+            case R.id.bt_recommend_second: {
                 setWhich(1);
                 setResultRecommend(which);
                 break;
             }
-            case R.id.bt_recommend_third:{
+            case R.id.bt_recommend_third: {
                 setWhich(2);
                 setResultRecommend(which);
                 break;
             }
-            case R.id.bt_recommend_fourth:{
+            case R.id.bt_recommend_fourth: {
                 setWhich(3);
                 setResultRecommend(which);
                 break;
@@ -146,7 +140,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }).start();
     }
-
     public void setResultRecommend(int which) {
         setRestaurant(restaurantArray[which]);
         setDishes(dishesArray[which]);
@@ -156,16 +149,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tvRecommendDishes.setText(demoDishes);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.backup: {
-                Toast.makeText(this, "This is our logo", Toast.LENGTH_SHORT).show();
-                break;
-            }
-
-            default:
-        }
-        return true;
-    }
 }
