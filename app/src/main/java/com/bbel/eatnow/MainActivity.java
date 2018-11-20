d9a128a68f0fec8d9e8a02a5ef8df8572511f46d
Dark-Existed <c2572388311@gmail.com>
2018-11-20 21:47:47 +0800

    faet: add base version custom map

--- app/src/main/java/com/bbel/eatnow/MainActivity.java
+++ app/src/main/java/com/bbel/eatnow/MainActivity.java
@@ -1,8 +1,12 @@
 package com.bbel.eatnow;
 
 import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.Manifest;
 import android.content.pm.PackageManager;
@@ -37,6 +41,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    DrawerLayout mDrawerLayout;
     MapView mapView;
     BaiduMap mBaiduMap;
     LocationClient mLocationClient;
@@ -62,9 +67,28 @@ public class MainActivity extends AppCompatActivity {
         mapView = findViewById(R.id.mmap);
         mBaiduMap = mapView.getMap();
 
+        mDrawerLayout = findViewById(R.id.main_drawer_layout);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.menu);
+        }
+
         bitmapDescriptor = BitmapDescriptorFactory.fromResource(R.drawable.location);
     }
 
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
 
     @Override
     protected void onResume() {
