e0dbf2651c8b8b139ad6ccbd584e51836a86b0aa
Benjamin <wangbin042@gmail.com>
2018-12-30 19:48:13 +0800

    chore:improve user convenience

--- app/src/main/java/com/bbel/eatnow/StoreLocationActivity.java
+++ app/src/main/java/com/bbel/eatnow/StoreLocationActivity.java
@@ -6,10 +6,15 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.bbel.eatnow.customview.MapView;
 
 import java.lang.ref.WeakReference;
+
+import static com.baidu.location.g.g.v;
 
 public class StoreLocationActivity extends BaseActivity {
 
@@ -21,16 +26,38 @@ public class StoreLocationActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store_location);
+        Button btn = findViewById(R.id.btn_home);
+
+        Toast.makeText(this, "抱歉，带我去功能尚未开发完成╮(╯▽╰)╭", Toast.LENGTH_SHORT).show();
         Intent intent = getIntent();
         String id = intent.getStringExtra("idRest");
         String map_id = intent.getStringExtra("canteenid");
         initWidgets();
         mapView.setChoose(id, mapIds[Integer.valueOf(map_id)]);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v)
+
+            {
+
+                Intent intentToHomePage = new Intent(StoreLocationActivity.this,MainActivity.class);
+                startActivity(intentToHomePage);
+
+
+            }
+
+        });
+
     }
 
 
     private void initWidgets() {
         mapView = findViewById(R.id.map);
     }
+
+
+
 
 }
